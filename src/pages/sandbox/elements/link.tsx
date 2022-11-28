fc236ac35bf465dfd6d43807997b243f7a712026
bizhermit <rsenda@bizhermit.com>
2022-11-28 18:46:41 +0900

    link

---
+++ src/pages/sandbox/elements/link.tsx
@@ -0,0 +1,24 @@
+import Button from "@/components/elements/button";
+import NextLink from "@/components/elements/link";
+import { NextPage } from "next";
+
+const Page: NextPage = () => {
+  return (
+    <div className="flex-box p-1 gap-1">
+      <NextLink href="/sandbox/elements/link">next link</NextLink>
+      <NextLink href="https://bizhermit.com" >normal link</NextLink>
+      <NextLink>no link</NextLink>
+      <NextLink href="/sandbox/elements/link">
+        <Button>next link</Button>
+      </NextLink>
+      <NextLink href="https://bizhermit.com" >
+        <Button>normal link</Button>
+      </NextLink>
+      <NextLink>
+        <Button>no link</Button>
+      </NextLink>
+    </div>
+  );
+};
+
+export default Page;
