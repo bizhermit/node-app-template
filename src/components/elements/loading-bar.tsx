5618ef2bb200f7d3d70635cbe7aae48252f97624
bizhermit <rsenda@bizhermit.com>
2022-11-29 22:24:59 +0900

    lint

--- src/components/elements/loading-bar.tsx
+++ src/components/elements/loading-bar.tsx
@@ -1,4 +1,4 @@
-import { attributes, attributesWithoutChildren } from "@/utilities/attributes";
+import { attributes } from "@/utilities/attributes";
 import React, { createContext, FC, HTMLAttributes, ReactNode, useCallback, useContext, useEffect, useRef, useState } from "react";
 import Style from "$/components/elements/loading-bar.module.scss";
 import usePortalElement from "@/hooks/portal-element";
