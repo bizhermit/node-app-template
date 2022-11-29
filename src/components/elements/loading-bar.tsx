5ed5b09f30fbf35dae724051138337b4049266d3
bizhermit <rsenda@bizhermit.com>
2022-11-29 22:12:29 +0900

    loadingbar

--- src/components/elements/loading-bar.tsx
+++ src/components/elements/loading-bar.tsx
@@ -1,17 +1,35 @@
 import { attributes } from "@/utilities/attributes";
-import React, { HTMLAttributes } from "react";
+import React, { FC, HTMLAttributes } from "react";
 import Style from "$/components/elements/loading-bar.module.scss";
+import usePortalElement from "@/hooks/portal-element";
+import { createPortal } from "react-dom";
 
 export type LoadingBarProps = HTMLAttributes<HTMLDivElement> & {
   $color?: Color;
+  $fixed?: boolean;
 };
 
 const LoadingBar = React.forwardRef<HTMLDivElement, LoadingBarProps>((props, ref) => {
   return (
-    <div {...attributes(props, Style.wrap)}>
-
+    <div
+      {...attributes(props, Style.wrap)}
+      ref={ref}
+      data-fixed={props.$fixed}
+    >
+      <div className={`${Style.bar} bgc-${props.$color || "main"}`} />
     </div>
   );
 });
 
 export default LoadingBar;
+
+export const ScreenLoadingBar = React.forwardRef<HTMLDivElement, LoadingBarProps>((props, ref) => {
+  const portal = usePortalElement({
+    mount: (elem) => {
+      elem.classList.add(Style.root);
+    }
+  });
+
+  if (portal == null) return <></>;
+  return createPortal(<LoadingBar {...props} ref={ref} $fixed />, portal);
+});
--- src/pages/sandbox/elements/loading-bar.tsx
+++ src/pages/sandbox/elements/loading-bar.tsx
@@ -1,12 +1,16 @@
-import LoadingBar from "@/components/elements/loading-bar";
+import LoadingBar, { ScreenLoadingBar } from "@/components/elements/loading-bar";
 import Row from "@/components/elements/row";
 import { colors } from "@/utilities/sandbox";
 import { NextPage } from "next";
 
 const Page: NextPage = () => {
   return (
-    <div className="flex-box flex-start">
+    <div className="flex-box flex-stretch w-100">
       <h1>LoadingBar</h1>
+      <section>
+        <h2>position</h2>
+        <ScreenLoadingBar />
+      </section>
       <section>
         <h2>color</h2>
         {colors.map(color => {
