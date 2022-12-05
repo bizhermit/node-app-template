090bdb528d8c9030f12579df3b351c4e8762d74f
bizhermit <rsenda@bizhermit.com>
2022-12-06 02:48:53 +0900

    debug

--- src/pages/sandbox/elements/badge.tsx
+++ src/pages/sandbox/elements/badge.tsx
@@ -1,26 +1,47 @@
 import Badge from "@/components/elements/badge";
+import Button from "@/components/elements/button";
+import Divider from "@/components/elements/divider";
+import RadioButtons from "@/components/elements/form-items/radio-buttons";
 import Row from "@/components/elements/row";
+import { sizes } from "@/utilities/sandbox";
 import { NextPage } from "next";
+import { useState } from "react";
 
 const Page: NextPage = () => {
+  const [count, setCount] = useState(99);
+  const [size, setSize] = useState<Size>("m");
+
   return (
     <div className="flex-box flex-start p-2 w-100 h-100 gap-1">
+      <Row className="gap-1">
+        <Button $onClick={() => setCount(c => c+1)}>count up</Button>
+        <Button $onClick={() => setCount(0)} $outline>reset</Button>
+        <RadioButtons
+          $source={sizes.map(size => {
+            return { value: size, label: size };
+          })}
+          $value={size}
+          $onChange={v => setSize(v!)}
+        />
+      </Row>
+      <Divider />
       <Row className="gap-1">
         <div className="anchor c-primary">
           <Badge
             className="c-base e-4 round"
             $position="left-top"
-            $size="m"
+            $size={size}
           >
-            0
+            {count}
           </Badge>
         </div>
         <div className="anchor c-primary">
           <Badge
             className="c-base e-4 round"
             $position="right-top"
+            $size={size}
           >
-            0
+            {count}
           </Badge>
         </div>
       </Row>
@@ -29,16 +50,18 @@ const Page: NextPage = () => {
           <Badge
             className="c-base e-4 round"
             $position="left-bottom"
+            $size={size}
           >
-            0
+            {count}
           </Badge>
         </div>
         <div className="anchor c-primary">
           <Badge
             className="c-base e-4 round"
             $position="right-bottom"
+            $size={size}
           >
-            0
+            {count}
           </Badge>
         </div>
       </Row>
