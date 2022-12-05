a04f30b355fc547e60f2af4198984d8112f2d288
bizhermit <rsenda@bizhermit.com>
2022-12-06 02:33:43 +0900

    pos, size

--- src/components/elements/badge.tsx
+++ src/components/elements/badge.tsx
@@ -1,17 +1,23 @@
 import { attributesWithoutChildren } from "@/utilities/attributes";
 import React, { HTMLAttributes, ReactNode } from "react";
 import Style from "$/components/elements/badge.module.scss";
+import LabelText from "@/components/elements/label-text";
 
 export type BadgeProps = HTMLAttributes<HTMLDivElement> & {
+  $position?: "left-top" | "right-top" | "left-bottom" | "right-bottom";
+  $size?: Size;
   children?: ReactNode;
 };
 
 const Badge = React.forwardRef<HTMLDivElement, BadgeProps>((props, ref) => {
   return (
     <div
-      {...attributesWithoutChildren(props, Style.wrap)}
+      {...attributesWithoutChildren(props, Style.main)}
+      ref={ref}
+      data-size={props.$size || "m"}
+      data-pos={props.$position || "right-top"}
     >
-
+      <LabelText>{props.children}</LabelText>
     </div>
   );
 });
--- src/pages/sandbox/elements/badge.tsx
+++ src/pages/sandbox/elements/badge.tsx
@@ -1,14 +1,56 @@
 import Badge from "@/components/elements/badge";
+import Row from "@/components/elements/row";
 import { NextPage } from "next";
 
 const Page: NextPage = () => {
   return (
-    <div className="flex-box flex-start p-1 w-100 h-100">
-      <div className="c-primary" style={{ height: 300, width: 300, position: "relative" }}>
-        <Badge>0</Badge>
-      </div>
+    <div className="flex-box flex-start p-2 w-100 h-100 gap-1">
+      <Row className="gap-1">
+        <div className="anchor c-primary">
+          <Badge
+            className="c-base e-4 round"
+            $position="left-top"
+            $size="m"
+          >
+            0
+          </Badge>
+        </div>
+        <div className="anchor c-primary">
+          <Badge
+            className="c-base e-4 round"
+            $position="right-top"
+          >
+            0
+          </Badge>
+        </div>
+      </Row>
+      <Row className="gap-1">
+        <div className="anchor c-primary">
+          <Badge
+            className="c-base e-4 round"
+            $position="left-bottom"
+          >
+            0
+          </Badge>
+        </div>
+        <div className="anchor c-primary">
+          <Badge
+            className="c-base e-4 round"
+            $position="right-bottom"
+          >
+            0
+          </Badge>
+        </div>
+      </Row>
+      <style jsx>{`
+        .anchor {
+          position: relative;
+          height: 20rem;
+          width: 20rem;
+        }
+      `}</style>
     </div>
-  )
+  );
 };
 
 export default Page;
