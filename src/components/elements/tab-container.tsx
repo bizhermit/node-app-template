8c436b0622ed6216ecbf464d7737581a4e1c61b8
bizhermit <rsenda@bizhermit.com>
2022-12-07 22:45:22 +0900

    tab animation

--- src/components/elements/tab-container.tsx
+++ src/components/elements/tab-container.tsx
@@ -1,39 +1,51 @@
 import { attributesWithoutChildren } from "@/utilities/attributes";
-import React, { FC, HTMLAttributes, Key, ReactElement, ReactNode, useState } from "react";
+import React, { FC, HTMLAttributes, Key, ReactElement, ReactNode, useEffect, useRef, useState } from "react";
 import Style from "$/components/elements/tab-container.module.scss";
 import LabelText from "@/components/elements/label-text";
 
 export type TabContainerProps = Omit<HTMLAttributes<HTMLDivElement>, "children"> & {
   $tabPosition?: "top" | "left" | "right" | "bottom";
   $defaultKey?: Key;
+  $color?: Color;
   children?: ReactElement | [ReactElement, ...Array<ReactElement>];
 };
 
 const TabContainer = React.forwardRef<HTMLDivElement, TabContainerProps>((props, ref) => {
+  const color = props.$color || "main";
+
   const [key, setKey] = useState(() => {
     if (props.$defaultKey != null) return props.$defaultKey;
     const firstContent = Array.isArray(props.children) ? props.children[0] : props.children!;
     return firstContent.key;
   });
 
-  const { tabs, body } = (() => {
+  const { tabs, bodys } = (() => {
     const children = Array.isArray(props.children) ? props.children : [props.children];
-    return {
-      tabs: children.map(child => {
-        const selected = child?.key === key;
-        const color: Color = child?.props.color || "main";
-        return (
-          <div
-            className={`${Style.tab} bdc-${color}${selected ? "" : ` c-${color}`}`}
-            data-selected={selected}
-            onClick={() => setKey(child?.key!)}
-          >
-            <LabelText>{child?.props.label}</LabelText>
-          </div>
-        );
-      }),
-      body: children.find(child => child?.key === key),
-    };
+    const tabs: Array<ReactNode> = [];
+    const bodys: Array<ReactNode> = [];
+    for (let i = 0, il = children.length; i < il; i++) {
+      const child = children[i]!;
+      const selected = child.key === key;
+      tabs.push(
+        <div
+          key={child.key}
+          className={`${Style.tab} bdc-${color}${selected ? ` c-${color}` : ""}`}
+          data-selected={selected}
+          onClick={() => setKey(child?.key!)}
+        >
+          <LabelText>{child?.props.label}</LabelText>
+        </div>
+      );
+      bodys.push(
+        <Content
+          key={child.key}
+          selected={selected}
+        >
+          {child}
+        </Content>
+      );
+    }
+    return { tabs, bodys };
   })();
 
   return (
@@ -45,17 +57,40 @@ const TabContainer = React.forwardRef<HTMLDivElement, TabContainerProps>((props,
       <div className={Style.header}>
         {tabs}
       </div>
+      <div className={`${Style.divider} bgc-${color}`} />
       <div className={Style.body}>
-        {body}
+        {bodys}
       </div>
     </div>
   );
 });
 
+const Content: FC<{
+  selected: boolean;
+  children: ReactNode;
+}> = (props) => {
+  const mounted = useRef(props.selected);
+  const [selected, setSelected] = useState(props.selected);
+
+  useEffect(() => {
+    if (props.selected) mounted.current = true;
+    setSelected(props.selected);
+  }, [props.selected]);
+
+  return (
+    <div
+      className={Style.content}
+      data-preselected={props.selected}
+      data-selected={selected}
+    >
+      {mounted.current && props.children}
+    </div>
+  );
+};
+
 export const TabContent: FC<{
   key: Key;
   label: ReactNode;
-  color?: Color;
   children?: ReactNode;
 }> = ({ children }) => {
   return <>{children}</>;
--- src/pages/sandbox/elements/container/tab-container.tsx
+++ src/pages/sandbox/elements/container/tab-container.tsx
@@ -2,8 +2,10 @@ import Divider from "@/components/elements/divider";
 import RadioButtons from "@/components/elements/form-items/radio-buttons";
 import Row from "@/components/elements/row";
 import TabContainer, { TabContent } from "@/components/elements/tab-container";
+import ArrayUtils from "@bizhermit/basic-utils/dist/array-utils";
 import { NextPage } from "next";
 import { useState } from "react";
+import { VscBrowser } from "react-icons/vsc";
 
 const Page: NextPage = () => {
   const [position, setPosition] = useState<"top" | "left" | "right" | "bottom">(null!);
@@ -33,12 +35,33 @@ const Page: NextPage = () => {
           label="Tab 1"
         >
           <h1>Tab 1</h1>
+          {ArrayUtils.generateArray(10, (idx) => (
+            <Row key={idx}>
+              <h2>piyo {idx}</h2>
+            </Row>
+          ))}
         </TabContent>
         <TabContent
           key="tab2"
           label="Tab 2"
         >
           <h1>Tab 2</h1>
+          {ArrayUtils.generateArray(10, (idx) => (
+            <Row key={idx}>
+              <h2>fuga {idx}</h2>
+            </Row>
+          ))}
+        </TabContent>
+        <TabContent
+          key="tab3"
+          label={<Row><VscBrowser /><span>Browser</span></Row>}
+        >
+          <h1>Tab 3</h1>
+          {ArrayUtils.generateArray(10, (idx) => (
+            <Row key={idx}>
+              <h2>hoge {idx}</h2>
+            </Row>
+          ))}
         </TabContent>
       </TabContainer>
     </div>
