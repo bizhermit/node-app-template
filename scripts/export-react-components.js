4ad2d30f5a0e182a3fb82f3d951df54ab7fcc085
bizhermit <rsenda@bizhermit.com>
2022-12-13 01:45:21 +0900

    replace path relative

--- scripts/export-react-components.js
+++ scripts/export-react-components.js
@@ -1,1 +1,43 @@
-console.log("// TODO: replace path");
+const path = require("path");
+const fse = require("fs-extra");
+const { isEmpty } = require("@bizhermit/basic-utils/dist/string-utils");
+
+const projectRoot = path.join(__dirname, "../");
+const root = path.join(projectRoot, "/dist/libs/react-components");
+if (!fse.existsSync(root)) {
+  process.stdout.write(`no directory: ${root}\n`);
+  process.exit();
+}
+
+fse.copySync(path.join(projectRoot, "src/styles"), path.join(root, "src/styles"));
+fse.copySync(path.join(projectRoot, "src/index.d.ts"), path.join(root, "src/index.d.ts"));
+fse.copySync(path.join(projectRoot, "src/styles.d.ts"), path.join(root, "src/styles.d.ts"));
+
+const impl = (basePath = root, nest = 0) => {
+  const dirs = fse.readdirSync(basePath);
+  dirs.forEach(dir => {
+    const name = path.join(basePath, dir);
+    const stat = fse.statSync(name);
+    if (stat.isDirectory()) {
+      impl(name, nest + 1);
+      return;
+    }
+    const ext = path.extname(name);
+    if (isEmpty(ext)) return;
+    const nestPath = path.join(root, "src").replace(/\\/g, "/");
+    const regExp = new RegExp(`(?<=(?:require\\(|@use\\s)")(${nestPath.replace(/\//g, "\\/").replace("\:", "\\:")})([^"]*)(?="(?:\\)|))`, "g");
+    if (ext === ".js" || ext === ".scss") {
+      let content = fse.readFileSync(name).toString()
+        .replace(/(?<=(?:require\(|@use\s)")(@\/)([^"]*)(?="(?:\)|))/g, `${nestPath}/$2`)
+        .replace(/(?<=(?:require\(|@use\s)")(\$\/)([^"]*)(?="(?:\)|))/g, `${nestPath}/styles/$2`);
+      content.match(regExp)?.forEach(item => {
+        let relativePath = path.relative(basePath, item).replace(/\\/g, "/");
+        if (!relativePath.startsWith(".")) relativePath = `./${relativePath}`;
+        content = content.replace(item, relativePath);
+      });
+      fse.writeFileSync(name, content);
+    }
+  });
+};
+
+impl();
