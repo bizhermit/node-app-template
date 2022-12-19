72cc13da48f8cf74544c5b29f9cd8c2ecd4ed71d
bizhermit <rsenda@bizhermit.com>
2022-12-19 20:06:25 +0900

    change func

--- src/components/elements/form-items/time-box.tsx
+++ src/components/elements/form-items/time-box.tsx
@@ -40,6 +40,9 @@ const TimeBox = React.forwardRef<HTMLDivElement, TimeBoxProps>((props, ref) => {
   const cacheH = useRef<number>();
   const cacheM = useRef<number>();
   const cacheS = useRef<number>();
+  const needH = type !== "ms";
+  const needM = type !== "h";
+  const needS = type === "hms" || type === "ms";
 
   const setInputValues = (value?: TimeValue) => {
     const time = convertTime(value, unit);
@@ -50,9 +53,6 @@ const TimeBox = React.forwardRef<HTMLDivElement, TimeBoxProps>((props, ref) => {
   });
 
   const commitCache = () => {
-    const needH = type !== "ms";
-    const needM = type !== "h";
-    const needS = type === "hms" || type === "ms";
     const h = needH ? cacheH.current : 0;
     const m = needM ? cacheM.current : 0;
     const s = needS ? cacheS.current : 0;
@@ -62,6 +62,136 @@ const TimeBox = React.forwardRef<HTMLDivElement, TimeBoxProps>((props, ref) => {
     }
     const time = new Time(`${h}:${m}:${s}`);
     form.change(TimeUtils.convertMillisecondsToUnit(time.getTime(), unit));
+  };
+
+  const changeH = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!form.editable) return;
+    const v = e.currentTarget.value;
+    if (!isNumericOrEmpty(v)) {
+      e.currentTarget.value = String(cacheH.current || "");
+      return;
+    }
+    cacheH.current = isEmpty(v) ? undefined : Number(v);
+    if (v.length === 2) mref.current?.focus();
+  };
+
+  const changeM = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!form.editable) return;
+    const v = e.currentTarget.value;
+    if (!isNumericOrEmpty(v)) {
+      e.currentTarget.value = String(cacheM.current || "");
+      return;
+    }
+    cacheM.current = isEmpty(v) ? undefined : Number(v);
+    if (v.length === 2 || !(v === "1" || v === "2")) mref.current?.focus();
+  };
+
+  const changeS = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!form.editable) return;
+    const v = e.currentTarget.value;
+    if (!isNumericOrEmpty(v)) {
+      e.currentTarget.value = String(cacheS.current || "");
+      return;
+    }
+    cacheS.current = isEmpty(v) ? undefined : Number(v);
+  };
+
+  const updown = (y = 0, m = 0, d = 0) => {
+    let hour = cacheH.current == null ? 0 : cacheH.current + y;
+    let minute = cacheM.current == null ? 0 : cacheM.current + m;
+    let second = cacheS.current == null ? 0 : cacheS.current + d;
+    // const date = new Date(hour, minute - 1, second);
+    // if (minDate) {
+    //   if (DatetimeUtils.isBeforeDate(minDate, date)) {
+    //     hour = minDate.getFullYear();
+    //     minute = minDate.getMonth() + 1;
+    //     second = minDate.getDate();
+    //   }
+    // }
+    // if (maxDate) {
+    //   if (DatetimeUtils.isAfterDate(maxDate, date)) {
+    //     hour = maxDate.getFullYear();
+    //     minute = maxDate.getMonth() + 1;
+    //     second = maxDate.getDate();
+    //   }
+    // }
+    if (!(cacheS.current !== second || cacheM.current !== minute || cacheH.current !== hour)) return;
+    cacheH.current = hour;
+    cacheM.current = minute;
+    cacheS.current = second;
+    commitCache();
+  };
+
+  const keydownH = (e: React.KeyboardEvent) => {
+    if (!form.editable) return;
+    switch (e.key) {
+      case "F2":
+        picker();
+        break;
+      case "Enter":
+        commitCache();
+        break;
+      case "ArrowUp":
+        updown(1, 0, 0);
+        e.preventDefault();
+        break;
+      case "ArrowDown":
+        updown(-1, 0, 0);
+        e.preventDefault();
+        break;
+      default:
+        break;
+    }
+  };
+
+  const keydownM = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (!form.editable) return;
+    switch (e.key) {
+      case "F2":
+        picker();
+        break;
+      case "Enter":
+        commitCache();
+        break;
+      case "Backspace":
+        if (e.currentTarget.value.length === 0) href.current?.focus();
+        break;
+      case "ArrowUp":
+        updown(0, 1, 0);
+        e.preventDefault();
+        break;
+      case "ArrowDown":
+        updown(0, -1, 0);
+        e.preventDefault();
+        break;
+      default:
+        break;
+    }
+  };
+
+  const keydownS = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (!form.editable) return;
+    switch (e.key) {
+      case "F2":
+        picker();
+        break;
+      case "Enter":
+        commitCache();
+        break;
+      case "Backspace":
+        if (e.currentTarget.value.length === 0) mref.current?.focus();
+        break;
+      case "ArrowUp":
+        updown(0, 0, 1);
+        e.preventDefault();
+        break;
+      case "ArrowDown":
+        updown(0, 0, -1);
+        e.preventDefault();
+        break;
+      default:
+        break;
+    }
   };
 
   const blur = (e: React.FocusEvent) => {
@@ -116,7 +246,7 @@ const TimeBox = React.forwardRef<HTMLDivElement, TimeBoxProps>((props, ref) => {
         onClick={clickInputs}
         data-input={!props.$disallowInput}
       >
-        {type !== "ms" &&
+        {needH &&
           <input
             ref={href}
             className={Style.h}
@@ -126,10 +256,12 @@ const TimeBox = React.forwardRef<HTMLDivElement, TimeBoxProps>((props, ref) => {
             maxLength={2}
             defaultValue={cacheH.current || ""}
             onFocus={focusInput}
+            onKeyDown={keydownH}
+            onChange={changeH}
           />
         }
         <span className={Style.sep} data-has={hasData}>:</span>
-        {type !== "h" &&
+        {needM &&
           <input
             ref={mref}
             className={Style.m}
@@ -139,9 +271,11 @@ const TimeBox = React.forwardRef<HTMLDivElement, TimeBoxProps>((props, ref) => {
             maxLength={2}
             defaultValue={cacheM.current || ""}
             onFocus={focusInput}
+            onKeyDown={keydownM}
+            onChange={changeM}
           />
         }
-        {(type === "hms" || type === "ms") &&
+        {needS &&
           <>
             <span className={Style.sep} data-has={hasData}>:</span>
             <input
@@ -153,6 +287,8 @@ const TimeBox = React.forwardRef<HTMLDivElement, TimeBoxProps>((props, ref) => {
               maxLength={2}
               defaultValue={cacheS.current || ""}
               onFocus={focusInput}
+              onKeyDown={keydownS}
+              onChange={changeS}
             />
           </>
         }
