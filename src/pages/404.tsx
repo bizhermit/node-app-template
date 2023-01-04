238a2cd4d19c56436b6454ec097a2bd8f0a0a559
bizhermit <rsenda@bizhermit.com>
2023-01-04 12:33:59 +0900

    404

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import Page404 from "@/components/404";
+import { NextPage } from "next";
+
+const Page: NextPage = () => {
+  return <Page404 className="w-100 h-100" />;
+};
+
+export default Page;
