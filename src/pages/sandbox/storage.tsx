704af0737d8f4f2cb2cb11174d072f75817ab63e
bizhermit <rsenda@bizhermit.com>
2023-01-26 09:43:13 +0900

    fix: local state

--- src/pages/sandbox/storage.tsx
+++ src/pages/sandbox/storage.tsx
@@ -1,10 +1,11 @@
 import Button from "@/components/elements/button";
 import Row from "@/components/elements/row";
-import { useSessionState } from "@/hooks/storage";
+import { useLocalState, useSessionState } from "@/hooks/storage";
 import { NextPage } from "next";
 
 const Page: NextPage = () => {
   const session = useSessionState("session", () => 10, { autoSave: true });
+  const local = useLocalState("local", () => 3);
 
   return (
     <div className="flex-stretch p-1">
@@ -15,14 +16,16 @@ const Page: NextPage = () => {
             session[1]((cur) => {
               return cur + 1;
             });
+            local[1]((cur) => cur + 1);
           }}
         >
-          count up {session[0]}
+          count up {session[0]}/{local[0]}
         </Button>
         <Button
           $onClick={() => {
             console.log("reset");
             session[1](0);
+            local[1](0);
           }}
         >
           reset
@@ -31,6 +34,7 @@ const Page: NextPage = () => {
           $onClick={() => {
             console.log("save");
             session[2].save();
+            local[2].save();
           }}
         >
           save
