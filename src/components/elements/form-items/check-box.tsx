5321a6aeb86700c4c8deeea5b5214edce416a6d4
rsenda <rsenda@bizhermit.com>
2023-03-07 12:15:03 +0900

    feat: checkbox outline

--- src/components/elements/form-items/check-box.tsx
+++ src/components/elements/form-items/check-box.tsx
@@ -2,7 +2,7 @@ import { convertDataItemValidationToFormItemValidation, type FormItemProps, Form
 import { type ForwardedRef, forwardRef, type FunctionComponent, type ReactElement, type ReactNode } from "react";
 import Style from "$/components/elements/form-items/check-box.module.scss";
 import Text from "@/components/elements/text";
-import { pressPositiveKey } from "@/components/utilities/attributes";
+import { joinClassNames, pressPositiveKey } from "@/components/utilities/attributes";
 
 export type CheckBoxProps<
   T extends string | number | boolean = boolean,
@@ -10,6 +10,7 @@ export type CheckBoxProps<
 > = Omit<FormItemProps<T, D>, "$tagPosition"> & {
   $checkedValue?: T;
   $uncheckedValue?: T;
+  $borderCheck?: boolean;
   $outline?: boolean;
   children?: ReactNode;
 };
@@ -100,10 +101,11 @@ const CheckBox: CheckBoxFC = forwardRef<HTMLDivElement, CheckBoxProps>(<
       $clickable
       $useHidden
       $mainProps={{
-        className: Style.main,
+        className: joinClassNames(Style.main, props.$outline && props.$color ? `bdc-${props.$color || "input"}` : undefined),
         onClick: click,
         onKeyDown: keydown,
         tabIndex: props.tabIndex ?? 0,
+        "data-outline": props.$outline,
       }}
     >
       <div className={Style.body}>
@@ -112,12 +114,12 @@ const CheckBox: CheckBoxFC = forwardRef<HTMLDivElement, CheckBoxProps>(<
           data-editable={ctx.editable}
         />
         <div
-          className={`${Style.check} ${props.$outline ? `bdc-${props.$color || "input"}` : `bdc-${props.$color || "main"}_r bgc-${props.$color || "main"}`}`}
+          className={`${Style.check} ${props.$borderCheck ? `bdc-${props.$color || "input"}` : `bdc-${props.$color || "main"}_r bgc-${props.$color || "main"}`}`}
           data-checked={ctx.value === checkedValue}
         />
       </div>
       {props.children &&
-        <Text>{props.children}</Text>
+        <Text className={Style.label}>{props.children}</Text>
       }
     </FormItemWrap>
   );
