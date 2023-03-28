f91393afeb9c03fef829770008f701c239297859
rsenda <rsenda@bizhermit.com>
2023-03-28 22:37:45 +0900

    fix: query param only string conversion/validation is implemented by user

--- src/hooks/query-param.ts
+++ src/hooks/query-param.ts
@@ -2,34 +2,18 @@ import type { NextComponentType, NextPageContext } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-type QueryDataType = "string" | "number";
-type QueryDataConvert<T extends QueryDataType = "string"> =
-  T extends "number" ? number :
-  string
-  ;
-
 export const pickQueryString = (val: string | string[] | undefined) => {
   const v = Array.isArray(val) ? val[0] : val;
   if (v == null || v === "") return null!;
   return v;
 };
 
-const convertValue = (val: string | null | undefined, dt: QueryDataType) => {
-  if (val == null) return val;
-  switch (dt) {
-    case "number":
-      return Number(val);
-    default:
-      return val;
-  }
-};
-
-export const useQueryParam = (pageProps: Struct, dataName = "id", dataType: QueryDataType = "string") => {
+export const useQueryParam = <S extends string = string>(pageProps: Struct, dataName = "id", init?: S | (() => S)) => {
   const router = useRouter();
-  const state = useState<QueryDataConvert<typeof dataType> | null | undefined>(convertValue(pageProps?.[dataName], dataType));
+  const state = useState<S>(pageProps?.[dataName] ?? init);
 
   useEffect(() => {
-    state[1](convertValue(pickQueryString(router.query[dataName]), dataType));
+    state[1](pickQueryString(router.query[dataName]) as S ?? init!);
   }, [router.query[dataName]]);
 
   return state;
