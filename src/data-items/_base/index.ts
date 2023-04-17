0506a02ad3d26ab183220141db4dede1bd5200e0
rsenda <rsenda@bizhermit.com>
2023-04-17 10:05:24 +0900

    refact: dataItem arg type

--- src/data-items/_base/index.ts
+++ src/data-items/_base/index.ts
@@ -1,7 +1,11 @@
 export const dataItemKey: DataItemKey = "$$";
 
-const dataItem = <C extends Omit<DataItem, DataItemKey>>(ctx?: C): Readonly<C extends (undefined | null) ? DataItem : C & DataItem> => {
-  return Object.freeze({ ...(ctx as any), [dataItemKey]: undefined });
+const dataItem = <
+  C extends Omit<DataItem, DataItemKey>
+>(ctx?: Readonly<C>) => {
+  return Object.freeze<C & Readonly<{
+    [dataItemKey]: undefined;
+  }>>({ ...(ctx as any), [dataItemKey]: undefined });
 };
 
 export default dataItem;
