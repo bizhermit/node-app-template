8b934d1f0bf533a20399d2fd7c050af416d24a25
rsenda <rsenda@bizhermit.com>
2023-05-17 10:00:04 +0900

    feat: link-button

--- src/components/elements/link.tsx
+++ src/components/elements/link.tsx
@@ -1,37 +1,38 @@
 import { attributes, isReactNode } from "@/components/utilities/attributes";
 import Link, { type LinkProps } from "next/link";
 import type { UrlObject } from "url";
-import type { AnchorHTMLAttributes, ElementType, FC, ReactNode } from "react";
+import { type AnchorHTMLAttributes, type ElementType, type LegacyRef, type ReactNode, forwardRef, type Ref } from "react";
 import StringUtils from "@bizhermit/basic-utils/dist/string-utils";
 
 export type NextLinkProps = Omit<LinkProps, "href"> & Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "href"> & {
   href?: string | UrlObject;
-  $tag?: ElementType;
+  $altTag?: ElementType;
   $noDecoration?: boolean;
   $disabled?: boolean;
   children?: ReactNode;
 };
 
-const NextLink: FC<NextLinkProps> = (props) => {
+const NextLink = forwardRef<HTMLElement, NextLinkProps>((props, ref) => {
   const attrs = attributes(props, props.$noDecoration ? "no-decoration" : undefined);
   const href = attrs.href?.toString();
 
   if (StringUtils.isEmpty(href) || props.$disabled) {
-    const Tag = props.$tag ?? (isReactNode(props.children) ? "div" : "span");
-    return <Tag {...attrs} />;
+    const Tag = props.$altTag ?? (isReactNode(props.children) ? "div" : "span");
+    return <Tag {...attrs} ref={ref} />;
   }
 
   if (/^(http|tel:|mailto:)/.test(href)) {
     return (
       <a
         {...attrs}
+        ref={ref as LegacyRef<HTMLAnchorElement> | undefined}
         href={href}
         target={props.target ?? "_blank"}
         rel={props.rel ?? "noopener noreferrer"}
       />
     );
   }
-  return <Link {...attrs} href={attrs.href!} />;
-};
+  return <Link {...attrs} href={attrs.href!} ref={ref as Ref<HTMLAnchorElement> | undefined} />;
+});
 
 export default NextLink;
