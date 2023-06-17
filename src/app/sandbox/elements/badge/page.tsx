88672a85c3c2721fe69db20dcc75249f97ecfcb1
rsenda <rsenda@bizhermit.com>
2023-06-17 16:08:43 +0900

    refact

--- src/app/sandbox/elements/badge/page.tsx
+++ src/app/sandbox/elements/badge/page.tsx
@@ -1,8 +1,18 @@
+import Badge from "#/components/elements/badge";
 import BadgeClient from "@/sandbox/elements/badge/client";
 
 const Page = () => {
   return (
-    <BadgeClient />
+    <>
+      <Badge
+        $size="s"
+      >
+        <div className="w-100 h-100 c-main round flex-center">
+          0
+        </div>
+      </Badge>
+      <BadgeClient />
+    </>
   );
 };
 
