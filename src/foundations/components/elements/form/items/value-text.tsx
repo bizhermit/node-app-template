d436a6c0a53faad2986c4de4166cb637bc0346e0
rsenda <rsenda@bizhermit.com>
2023-08-21 20:54:52 +0900

    feat: add ValueText form value disp as text.

---
+++ src/foundations/components/elements/form/items/value-text.tsx
@@ -0,0 +1,30 @@
+import type { FormItemProps, ValueType } from "#/components/elements/form/$types";
+import useForm from "#/components/elements/form/context";
+import { useDataItemMergedProps } from "#/components/elements/form/item-hook";
+import Text from "#/components/elements/text";
+import { attributes } from "#/components/utilities/attributes";
+import type { ReactNode } from "react";
+
+export type ValueTextProps<
+  T extends string | number | boolean | Date | null | undefined = string | number | boolean | Date | null | undefined,
+  D extends DataItem_String | DataItem_Number | DataItem_Boolean | DataItem_Date | undefined = undefined
+> = FormItemProps<string | number | boolean, D, undefined> & {
+  $format?: (v: ValueType<T, D, undefined>) => ReactNode;
+};
+
+const ValueText = <
+  T extends string | number | boolean | Date | null | undefined = string | number | boolean | Date | null | undefined,
+  D extends DataItem_String | DataItem_Number | DataItem_Boolean | DataItem_Date | undefined = undefined
+>(p: ValueTextProps<T, D>) => {
+  const form = useForm();
+  const props = useDataItemMergedProps(form, p);
+  const value = props.name ? form.getValue(props.name) : undefined;
+
+  return (
+    <Text {...attributes(props)}>
+      {props.$format ? props.$format(value as any) : String(value ?? "")}
+    </Text>
+  );
+};
+
+export default ValueText;
