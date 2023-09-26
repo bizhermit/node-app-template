45526c82a3336b380653d12be71d9b5ff4319eec
rsenda <rsenda@bizhermit.com>
2023-09-26 09:59:36 +0900

    fix: window provider listener in suspense

--- src/foundations/client/providers/window/provider.tsx
+++ src/foundations/client/providers/window/provider.tsx
@@ -1,26 +1,18 @@
 "use client";
 
-import { useParams, usePathname, useSearchParams } from "next/navigation";
-import { useEffect, useMemo, useRef, type FC, type ReactNode } from "react";
+import WindowProviderEventListener from "#/client/providers/window/listener";
+import { Suspense, useMemo, useRef, type FC, type ReactNode } from "react";
 import { windowOpen } from "../../utilities/window-open";
 import { WindowContext } from "./context";
 
 export const WindowProvider: FC<{
   children?: ReactNode;
 }> = ({ children }) => {
-  const pathname = usePathname();
-  const params = useParams();
-  const searchParams = useSearchParams();
   const wins = useRef<Array<ReturnType<typeof windowOpen>>>([]);
 
   const append = (ctx: ReturnType<typeof windowOpen>) => {
     wins.current.push(ctx);
   };
-
-  useEffect(() => {
-    wins.current.forEach(win => win.close());
-    wins.current = wins.current.filter(win => win.showed());
-  }, [pathname, JSON.stringify(params), searchParams?.toString()]);
 
   return (
     <WindowContext.Provider
@@ -30,6 +22,9 @@ export const WindowProvider: FC<{
         }, [])
       }
     >
+      <Suspense>
+        <WindowProviderEventListener wins={wins} />
+      </Suspense>
       {children}
     </WindowContext.Provider>
   );
