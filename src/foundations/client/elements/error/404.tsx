f7085d64952e026cb0dcaea552939b9358aedb70
rsenda <rsenda@bizhermit.com>
2023-10-13 16:52:47 +0900

    fix: 404 component

---
+++ src/foundations/client/elements/error/404.tsx
@@ -0,0 +1,19 @@
+import NextLink from "#/client/elements/link";
+import { type FC, type HTMLAttributes } from "react";
+import { attributes } from "../../utilities/attributes";
+import Style from "./index.module.scss";
+
+const Error404: FC<HTMLAttributes<HTMLDivElement>> = (props) => {
+  return (
+    <div {...attributes(props, Style.wrap)}>
+      <h1 className={Style.title}>404&nbsp;|&nbsp;Not&nbsp;Found</h1>
+      {props.children ??
+        <NextLink href="/">
+          back
+        </NextLink>
+      }
+    </div>
+  );
+};
+
+export default Error404;
