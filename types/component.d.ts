590eda50c725470bc28f35ce0ee12f14bf27bb61
rsenda <rsenda@bizhermit.com>
2023-10-19 11:52:00 +0900

    refact: types

--- types/component.d.ts
+++ types/component.d.ts
@@ -5,34 +5,38 @@ type CommonStyleProps = {
 
 type CFC<P extends { [key: string]: any } = { [key: string]: any }> = React.FC<P & { children?: React.ReactNode; }>;
 
+type QueryString = undefined | string | string[];
+type QueryParams = { [key: string]: QueryString };
+type ReturnNode = React.ReactNode | React.ReactElement;
+
 type LayoutWithSlotsFC<
   T extends string = string,
-  P extends { [key: string]: undefined | string | string[] } | undefined = { [key: string]: undefined | string | string[] } | undefined,
-  S extends { [key: string]: undefined | string | string[] } | undefined = { [key: string]: undefined | string | string[] } | undefined
+  P extends QueryParams | undefined = QueryParams | undefined,
+  S extends QueryParams | undefined = QueryParams | undefined
 > = (props: {
   params: P;
   searchParams: S;
   children: React.ReactNode
-} & Record<T, React.ReactNode>) => (React.ReactNode | React.ReactElement | Promise<React.ReactNode | React.ReactElement>);
+} & Record<T, React.ReactNode>) => (ReturnNode | Promise<ReturnNode>);
 
 type LayoutFC<
-  P extends { [key: string]: undefined | string | string[] } | undefined = { [key: string]: undefined | string | string[] } | undefined,
-  S extends { [key: string]: undefined | string | string[] } | undefined = { [key: string]: undefined | string | string[] } | undefined
+  P extends QueryParams | undefined = QueryParams | undefined,
+  S extends QueryParams | undefined = QueryParams | undefined
 > = (props: {
   params: P;
   searchParams: S;
   children: React.ReactNode
-}) => (React.ReactNode | React.ReactElement | Promise<React.ReactNode | React.ReactElement>);
+}) => (ReturnNode | Promise<ReturnNode>);
 
 type PageFC<
-  P extends { [key: string]: undefined | string | string[] } | undefined = { [key: string]: undefined | string | string[] } | undefined,
-  S extends { [key: string]: undefined | string | string[] } | undefined = { [key: string]: undefined | string | string[] } | undefined
+  P extends QueryParams | undefined = QueryParams | undefined,
+  S extends QueryParams | undefined = QueryParams | undefined
 > = (props: {
   params: P;
   searchParams: S;
-}) => (React.ReactNode | React.ReactElement | Promise<React.ReactNode | React.ReactElement>);
+}) => (ReturnNode | Promise<ReturnNode>);
 
 type ErrorFC = (props: {
   error: Error;
   reset: () => void;
-}) => (React.ReactNode | React.ReactElement);
+}) => (ReturnNode);
