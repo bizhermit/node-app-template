f076f0287a16dd554515b4dbcfbf74c132f5e284
rsenda <rsenda@bizhermit.com>
2023-12-18 10:10:58 +0900

    fix: 404 attrs

--- foundations/client/elements/error/404.tsx
+++ foundations/client/elements/error/404.tsx
@@ -1,11 +1,11 @@
 import { type FC, type HTMLAttributes } from "react";
-import { attributes } from "../../utilities/attributes";
+import { attrs } from "../../utilities/attributes";
 import NextLink from "../link";
 import Style from "./index.module.scss";
 
 const Error404: FC<HTMLAttributes<HTMLDivElement>> = (props) => {
   return (
-    <div {...attributes(props, Style.wrap)}>
+    <div {...attrs(props, Style.wrap)}>
       <h1 className={Style.title}>404&nbsp;|&nbsp;Not&nbsp;Found</h1>
       {props.children ??
         <NextLink href="/">
