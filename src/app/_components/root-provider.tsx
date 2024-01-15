a3aadd7c99e623326c6e08c2e097f28eab701a43
rsenda <rsenda@bizhermit.com>
2024-01-15 14:24:42 +0900

    feat: session provider

--- src/app/_components/root-provider.tsx
+++ src/app/_components/root-provider.tsx
@@ -4,18 +4,21 @@ import LoadingProvider from "#/client/elements/loading/provider";
 import LayoutProvider from "#/client/providers/layout/provider";
 import MessageProvider from "#/client/providers/message/provider";
 import WindowProvider from "#/client/providers/window/provider";
+import { SessionProvider } from "next-auth/react";
 
 const RootProvider: CFC = ({ children }) => {
   return (
-    <WindowProvider>
-      <LayoutProvider>
-        <MessageProvider>
-          <LoadingProvider>
-            {children}
-          </LoadingProvider>
-        </MessageProvider>
-      </LayoutProvider>
-    </WindowProvider>
+    <SessionProvider>
+      <WindowProvider>
+        <LayoutProvider>
+          <MessageProvider>
+            <LoadingProvider>
+              {children}
+            </LoadingProvider>
+          </MessageProvider>
+        </LayoutProvider>
+      </WindowProvider>
+    </SessionProvider>
   );
 };
 
