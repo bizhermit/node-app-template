1495b5a816e2efa445bbdd8abf583b9bb899ac4c
rsenda <rsenda@bizhermit.com>
2024-01-22 12:08:28 +0900

    refact

--- src/foundations/objects/struct/entries.ts
+++
@@ -1,5 +0,0 @@
-const structEntries = <U extends { [v: string | number | symbol]: any }>(obj: U | null | undefined): Array<[keyof U, U[keyof U]]> => {
-  return obj ? Object.keys(obj).map(k => [k, obj[k]]) : [];
-};
-
-export default structEntries;
