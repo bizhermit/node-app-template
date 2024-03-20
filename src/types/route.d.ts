890eac321e166ed06d2c166eb8b8a2f296106b35
bizhermit <rsenda@bizhermit.com>
2024-03-20 09:36:44 +0000

    refact: sort route types file < dir

--- src/types/route.d.ts
+++ src/types/route.d.ts
@@ -1,12 +1,15 @@
 // generate by script
 // do not edit
 
-type AppRoutePath = "/[uid]"
+type AppRoutePath = "/"
+  | "/[uid]"
+  | "/dev"
   | "/dev/color"
   | "/dev/dynamic-route"
   | "/dev/dynamic-route/param/[slug]"
   | "/dev/dynamic-route/slug-param/[...slug]"
   | "/dev/dynamic-route/slugs-param/[[...slug]]"
+  | "/dev/elements"
   | "/dev/elements/badge"
   | "/dev/elements/button"
   | "/dev/elements/container/card"
@@ -16,14 +19,13 @@ type AppRoutePath = "/[uid]"
   | "/dev/elements/container/split"
   | "/dev/elements/container/tab"
   | "/dev/elements/divider"
+  | "/dev/elements/form"
   | "/dev/elements/form/item/check-list"
   | "/dev/elements/form/item/select-box"
-  | "/dev/elements/form"
   | "/dev/elements/icon"
   | "/dev/elements/label"
   | "/dev/elements/link"
   | "/dev/elements/loading"
-  | "/dev/elements"
   | "/dev/elements/popup"
   | "/dev/elements/stepper"
   | "/dev/elements/tooltip"
@@ -31,11 +33,10 @@ type AppRoutePath = "/[uid]"
   | "/dev/elements/view/data-table"
   | "/dev/elements/view/menu"
   | "/dev/elements/view/struct-view"
-  | "/dev/extensions/hoge"
   | "/dev/extensions"
+  | "/dev/extensions/hoge"
   | "/dev/fetch"
-  | "/dev"
-  | "/"
+  | "/sandbox"
   | "/sandbox/group-a"
   | "/sandbox/group-b"
   | "/sandbox/elements/form-items/check-box"
@@ -59,36 +60,35 @@ type AppRoutePath = "/[uid]"
   | "/sandbox/elements/form-items/toggle-switch"
   | "/sandbox/fetch"
   | "/sandbox/message-box"
-  | "/sandbox"
   | "/sandbox/post/sender"
   | "/sandbox/process"
   | "/sandbox/storage"
   | "/sandbox/window"
   | "/sign-in";
 
-type AppApiPath = "/api/auth/[...nextauth]"
+type AppApiPath = "/api"
+  | "/api/auth/[...nextauth]"
   | "/api/fetch"
-  | "/api"
-  | "/dev/fetch/api/dev"
-  | "/dev/fetch/api";
+  | "/dev/fetch/api"
+  | "/dev/fetch/api/dev";
 
 type TypeofAppApi = {
+  "/api": typeof import("app/api/route.ts");
   "/api/auth/[...nextauth]": typeof import("app/api/auth/[...nextauth]/route.ts");
   "/api/fetch": typeof import("app/api/fetch/route.ts");
-  "/api": typeof import("app/api/route.ts");
-  "/dev/fetch/api/dev": typeof import("app/dev/fetch/api/dev/route.dev.ts");
   "/dev/fetch/api": typeof import("app/dev/fetch/api/route.ts");
+  "/dev/fetch/api/dev": typeof import("app/dev/fetch/api/dev/route.dev.ts");
 };
 
 type PagesRoutePath = "/404"
   | "/pages"
   | "/root"
+  | "/sandbox/pages"
+  | "/sandbox/route"
   | "/sandbox/extensions"
   | "/sandbox/extensions/hoge"
   | "/sandbox/nest/[id]"
-  | "/sandbox/pages"
-  | "/sandbox/post/recipient"
-  | "/sandbox/route";
+  | "/sandbox/post/recipient";
 
 type PagesApiPath = "/api/form"
   | "/api/hello";
