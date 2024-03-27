fe164da6b48e99f7d2eb2ab897eee4321bb2d171
rsenda <rsenda@bizhermit.com>
2024-03-27 19:08:45 +0900

    feat: add next dev script

---
+++ scripts/next-app/dev.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DEV_CONT_ENV_FILE=".devcontainer/.env"
+if [ -f "$DEV_CONT_ENV_FILE" ]; then
+  source "$DEV_CONT_ENV_FILE"
+fi
+
+if [ -z "$NEXT_PORT" ]; then
+  export NEXT_PORT=3000
+fi
+
+if [ -z "$NEXTAUTH_URL" ]; then
+  export NEXTAUTH_URL="http://localhost:$NEXT_PORT"
+fi
+
+if [ -z "$NEXTAUTH_SECRET" ]; then
+  export NEXTAUTH_SECRET="secret"
+fi
+
+npm run clean
+npm run route
+npx next dev -p $NEXT_PORT
