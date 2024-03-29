a14397fd8e6f6f01396389cb7c9e49eefde32bb2
rsenda <rsenda@bizhermit.com>
2024-03-29 11:20:14 +0900

    feat: prod docker-compose use env-file opt

--- scripts/next-app/dev.sh
+++ scripts/next-app/dev.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-DEV_CONT_ENV_FILE=".devcontainer/.env"
-if [ -f "$DEV_CONT_ENV_FILE" ]; then
-  source "$DEV_CONT_ENV_FILE"
-fi
+# DEV_CONT_ENV_FILE=".devcontainer/.env"
+# if [ -f "$DEV_CONT_ENV_FILE" ]; then
+#   source "$DEV_CONT_ENV_FILE"
+# fi
 
-if [ -z "$NEXT_PORT" ]; then
-  export NEXT_PORT=3000
-fi
+# if [ -z "$NEXT_PORT" ]; then
+#   export NEXT_PORT=3000
+# fi
 
-if [ -z "$NEXTAUTH_URL" ]; then
-  export NEXTAUTH_URL="http://localhost:$NEXT_PORT"
-fi
+# if [ -z "$NEXTAUTH_URL" ]; then
+#   export NEXTAUTH_URL="http://localhost:$NEXT_PORT"
+# fi
 
-if [ -z "$NEXTAUTH_SECRET" ]; then
-  export NEXTAUTH_SECRET="secret"
-fi
+# if [ -z "$NEXTAUTH_SECRET" ]; then
+#   export NEXTAUTH_SECRET="secret"
+# fi
 
 npm run clean
 npm run route
